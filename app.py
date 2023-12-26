16dacf70799ced6ce7aad68d28b64e680212df55
atomtables <76572470+atomtables@users.noreply.github.com>
2023-12-25 21:33:20 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pam
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return '<h1>Hello World!</h1> <p>average yeet enjoyer</p>'
+
+
+@app.route('/authenticate/<username>/<password>')
+def lol(username: str, password: str):
+    pam.authenticate(username, password)
 
 
 if __name__ == "__main__":
