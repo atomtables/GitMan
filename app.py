7a72d420a4e782796b2cfdda2dd02497cec95c7d
atomtables <76572470+atomtables@users.noreply.github.com>
2023-12-25 21:17:25 -0500

    update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!</h1> <p>average yeet enjoyer</p>'
 
 if __name__ == "__main__":
     app.run()
