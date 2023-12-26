5249f9e3f8661565fd9beffd3cd928ace70e6594
atomtables <76572470+atomtables@users.noreply.github.com>
2023-12-26 12:11:13 -0500

    mainpage

---
+++ static/mainpageGradients.js
@@ -0,0 +1,20 @@
+function generateRandomColor(seed) {
+    const hash = seed.split('').reduce((acc, char) => char.charCodeAt(0) + (acc << 6) + (acc << 16) - acc, 0);
+    const baseColor = '#' + (hash & 0x00FFFFFF).toString(16).toUpperCase();
+    return adjustBrightness(baseColor, -120);
+}
+function adjustBrightness(color, amount) {
+    const clamp = (value, min, max) => Math.min(max, Math.max(min, value));
+    const hexToRgb = hex => [parseInt(hex.slice(1, 3), 16), parseInt(hex.slice(3, 5), 16), parseInt(hex.slice(5, 7), 16)];
+    const rgbToHex = (r, g, b) => `#${(1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1)}`;
+    const rgb = hexToRgb(color);
+    const adjustedRgb = rgb.map(value => clamp(value + amount, 0, 255));
+    return rgbToHex(...adjustedRgb);
+}
+function generateRandomColorStop(seed) {
+    const color = generateRandomColor(seed);
+    return `${color}`;
+}
+const gradientColorStops = Array.from({length: 5}, (_, index) => generateRandomColorStop(`${hostname}${index + Math.random() * 100}`));
+document.body.style.background = `linear-gradient(45deg, ${gradientColorStops.join(', ')})`;
+document.body.style.backgroundRepeat = 'no-repeat';
