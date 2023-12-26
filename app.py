dbd74b4affe0f0e6e48ce06864ca48f32c55a57b
atomtables <76572470+atomtables@users.noreply.github.com>
2023-12-26 09:32:10 -0500

    header and login

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from hashlib import sha256
 
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, request, abort
 import pam
 
 app = Flask(__name__)
@@ -11,10 +11,28 @@ def hello_world():  # put application's code here
     return '<h1>Hello World!</h1> <p>average yeet enjoyer</p>'
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if pam.authenticate(request.form['username'], request.form['password']):
+            resp = make_response("Success")
+            resp.set_cookie('username', request.form['username'])
+            resp.set_cookie('userhash',
+                            str(sha256(
+                                f"{request.form['username'][:1]} + {request.form['password']} + {request.form['username'][1:]}".encode(
+                                    'utf-8')).hexdigest()))
+
+            return resp
+        else:
+            abort(403)
+
+    return render_template('login.html')
+
+
 @app.route('/authenticate/<username>/<password>')
 def lol(username: str, password: str):
     if pam.authenticate(username, password):
-        resp = make_response("render_template()")
+        resp = make_response("Success")
         resp.set_cookie('username', username)
         resp.set_cookie('userhash', str(sha256(f"username[:1] + password + username[1:]".encode('utf-8')).hexdigest()))
         return resp
